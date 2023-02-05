4ad32ccd38f75b89268290c4e10aa24062760876
1606139 <arnauapps@gmail.com>
2023-02-05 20:58:53 +0100

    Added DB script and fixed recieving casteller data

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ const app: express.Application = express()
 const port:number = 4000
 
 app.use(cors());
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World Pinyator!')
 })
@@ -42,6 +43,12 @@ app.get('/addcasteller', async (req, res) => {
 
 app.post('/addcasteller', async (req,res) => {
   console.log(req.body);
+  const dadesCasteller = req.body;
+  let conn;
+  conn = await pool.getConnection();
+  const result: JSON = await conn.query("SELECT COUNT(*) AS numCastellers FROM CASTELLER");
+  //const id = result[0].numCastellers + 1; // Retorna el numero total de castellers, per el codi nou, sha de fer aquesta variable incrementada.
+  //const insert = await conn.query("INSERT INTO (MalNom, Nom, Cognom_1, Cognom_2, Altura, Forca, POSICIO_PINYA_ID, Codi, Familia_ID, Estat) VALUES ")
   res.send('Data recieved');
 })
 
