2e0461538c4f757641123273e188c2cd84347eb6
Aleix Trasserra Martinez <aleixtrasserramartinez@MacBook-Air-de-Aleix.local>
2023-02-14 12:38:53 +0100

    simplified codi generation

--- index.ts
+++ index.ts
@@ -47,12 +47,10 @@ app.post('/addcasteller', async (req,res) => {
   const dadesCasteller = req.body;
   let conn;
   conn = await pool.getConnection();
-  const result = await conn.query("SELECT COUNT(*) AS numCastellers FROM CASTELLER");
-  let nCastellers = Number(result[0].numCastellers); // Retorna el numero total de castellers en tipus bigInt, s'ha de passar a number
-  const id = nCastellers + 1; // Afegim 1 per fer la nova id.
+  
   const insertQuery = `INSERT INTO CASTELLER (MalNom, Nom, Cognom_1, Cognom_2, Altura, Forca, POSICIO_PINYA_ID, Codi, Familia_ID, Estat) VALUES
                                       ('${dadesCasteller.malNom}', '${dadesCasteller.nom}', '${dadesCasteller.cognom_1}', '${dadesCasteller.cognom_2}',
-                                      ${dadesCasteller.altura}, ${dadesCasteller.forca}, ${dadesCasteller.posicioPinya}, ${id}, 
+                                      ${dadesCasteller.altura}, ${dadesCasteller.forca}, ${dadesCasteller.posicioPinya}, '1', 
                                       ${dadesCasteller.family}, ${dadesCasteller.state})`;
   const insert = await conn.query(insertQuery);
   if(insert) console.log("Inserted data into DB.")
